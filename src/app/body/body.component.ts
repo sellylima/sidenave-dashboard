ddab5e91a7f3b49c4ba34284f0bb336396ee0e35
sellylima <vanessa.limanasc@gmail.com>
2024-08-31 15:48:46 -0300

    implement body

--- src/app/body/body.component.ts
+++ src/app/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-body',
@@ -8,5 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class BodyComponent implements OnInit {
   ngOnInit(): void {
   }
+  @Input() collapsed = false;
+  @Input() screenWidth = 0;
 
+  getBodyClass(): string {
+    let styleClass = '';
+    if(this.collapsed && this.screenWidth > 768) {
+      styleClass = 'body-trimmed';
+    } else if(this.collapsed && this.screenWidth <= 768 && this.screenWidth > 0) {
+      styleClass = 'body-md-screen'
+    }
+    return styleClass;
+  }
 }
